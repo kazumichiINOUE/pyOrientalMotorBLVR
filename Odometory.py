41da4085e9c6e652b0020f68597b6da642aa8e1f
KazumichiINOUE <kazumichiinoue@mail.saitama-u.ac.jp>
2024-08-29 11:27:47 +0900

    refactor: odometory class crated

---
+++ Odometory.py
@@ -0,0 +1,10 @@
+class Odometory():
+    def __init__(self):
+        self.dist_R = 0.0
+        self.dist_L = 0.0
+        self.travel = 0.0
+        self.rotation = 0.0
+        self.rx = 0.0
+        self.ry = 0.0
+        self.ra = 0.0
+
