66d6bd949076525772715e898f6105407d6c1e3b
KazumichiINOUE <kazumichiinoue@mail.saitama-u.ac.jp>
2024-09-09 11:36:59 +0900

    chore: update parameter

--- config.lua
+++ config.lua
@@ -10,7 +10,7 @@ local config = {
   lidar = {
     debug_mode  = false,
     store_data  = false,
-    serial_port = '/dev/cu.usbmodem113301',
+    serial_port = '/dev/cu.usbmodem13301',
     baudrate    = 115200,
     start_angle = -135.0,
     end_angle   =  135.0,
@@ -48,7 +48,7 @@ local config = {
     scan_data_size = 1081,
     skip = 32,
     -- offline SLAM settings as follows:
-    urg_log_file = "./urglog_2024_08_27_17_20_20",
+    urg_log_file = "./urglog_2024_09_02_10_25_49",
     result_path = "./result/path",
     result_gmap = "./result/result.png",
   },
