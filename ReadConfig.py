53f7b789456ab4b29330155f4894e6d49df6cbfd
KazumichiINOUE <kazumichiinoue@mail.saitama-u.ac.jp>
2025-03-20 16:36:04 +0900

    ReadConfig.py でmapInfoも読む

--- ReadConfig.py
+++ ReadConfig.py
@@ -8,3 +8,12 @@ def read_config(fname = 'config.lua'):
         _config = lua.execute(f.read())
     
     return _config
+    
+def read_mapInfo(fname = 'mapInfo.lua'):
+    # Luaインタープリタを初期化
+    lua = lupa.LuaRuntime()
+    # mapInfo.luaファイルを読み込む
+    with open(fname, 'r', encoding='utf-8') as f:
+        _config = lua.execute(f.read())
+    
+    return _config
