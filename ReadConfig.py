4a3bc421bd0e712954a6de0e81245717c21ebc66
KazumichiINOUE <kazumichiinoue@mail.saitama-u.ac.jp>
2025-03-28 17:31:25 +0900

    wpの読み込み

--- ReadConfig.py
+++ ReadConfig.py
@@ -17,3 +17,17 @@ def read_mapInfo(fname = 'mapInfo.lua'):
         _config = lua.execute(f.read())
     
     return _config
+
+def read_wp(fname = 'wp_list.lua'):
+    # Luaインタープリタを初期化
+    lua = lupa.LuaRuntime(unpack_returned_tuples=True)
+    # Luaスクリプトを読み込む
+    with open(fname, 'r', encoding='utf-8') as f:
+        lua.execute(f.read())
+    # Luaのグローバル変数 `wp_list` を取得
+    wp_list = lua.globals().wp_list
+    print(wp_list)
+    return wp_list
+
+    # Pythonのリストとして返す
+    return [list(wp) for wp in wp_list]
